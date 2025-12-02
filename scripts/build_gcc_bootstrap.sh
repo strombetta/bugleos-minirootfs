8e9949514e11875b1120bf86f9541fdae0d9dfdd
Sebastiano Trombetta <strombetta@users.noreply.github.com>
2025-12-02 22:26:03 +0100

    Update build_gcc_bootstrap.sh

--- scripts/build_gcc_bootstrap.sh
+++ scripts/build_gcc_bootstrap.sh
@@ -50,9 +50,4 @@ $SOURCE_DIR/configure \
 
 # Build the compiler first
 make all-gcc all-target-libgcc -j$(nproc)
-# make install-gcc install-target-libgcc
-
-# Build and install libgcc so that later stages (like musl) have the
-# compiler builtins they need during linking
-# make  -j$(nproc)
-# make 
+make install-gcc install-target-libgcc
