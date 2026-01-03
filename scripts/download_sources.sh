8636d9ee90ef322ec246c7ae44d3fff5f1df75e5
strombetta <12155693+strombetta@users.noreply.github.com>
2026-01-03 10:28:13 +0100

    Update download_sources.sh

--- scripts/download_sources.sh
+++ scripts/download_sources.sh
@@ -22,27 +22,6 @@
 
 set -eu
 
-TARGET=$1
-PREFIX=$2
-SYSROOT=$3
-ROOTFS=$4
-SOURCES=$5
-BUILD=$6
-BINUTILS_VERSION=$7
-GCC_VERSION=$8
-LINUX_VERSION=$9
-MUSL_VERSION=${10}
-BUSYBOX_VERSION=${11}
-
-# URLs
-BINUTILS_URL="https://ftp.gnu.org/gnu/binutils/binutils-${BINUTILS_VERSION}.tar.xz"
-GCC_URL="https://ftp.gnu.org/gnu/gcc/gcc-${GCC_VERSION}/gcc-${GCC_VERSION}.tar.xz"
-LINUX_URL="https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-${LINUX_VERSION}.tar.xz"
-MUSL_URL="https://musl.libc.org/releases/musl-${MUSL_VERSION}.tar.gz"
-BUSYBOX_URL="https://busybox.net/downloads/busybox-${BUSYBOX_VERSION}.tar.bz2"
-
-mkdir -p "${SOURCES}"
-
 download() {
     url=$1
     dest=$2
@@ -53,6 +32,3 @@ download() {
         wget -O "$dest" "$url"
     fi
 }
-
-download "$TOOLCHAIN_URL" "${SOURCES}/toolchain-${MUSL_VERSION}.tar.gz"
-download "$BUSYBOX_URL" "${SOURCES}/busybox-${BUSYBOX_VERSION}.tar.bz2"
