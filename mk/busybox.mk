54866f55726b5ba17720e1f0e554d20ba68d09bd
strombetta <12155693+strombetta@users.noreply.github.com>
2026-01-07 10:45:09 +0100

    Update busybox.mk

--- mk/busybox.mk
+++ mk/busybox.mk
@@ -26,7 +26,7 @@ include $(abspath $(dir $(THIS_MAKEFILE))/helpers.mk)
 BUSYBOX_VERSION ?= 1.37.0
 BUSYBOX_URL := https://busybox.net/downloads/busybox-$(BUSYBOX_VERSION).tar.bz2
 BUSYBOX_TAR := busybox-$(BUSYBOX_VERSION).tar.bz2
-BUSYBOX_TAR_PATH := $(DOWNLOADS_DIR)/$(TOOLCHAIN_TAR)
+BUSYBOX_TAR_PATH := $(DOWNLOADS_DIR)/$(BUSYBOX_TAR)
 BUSYBOX_SIG := https://busybox.net/downloads/busybox-$(BUSYBOX_VERSION).tar.bz2.sig
 BUSYBOX_SHA256 := 3311dff32e746499f4df0d5df04d7eb396382d7e108bb9250e7b519b837043a4
 BUSYBOX_DIR ?= $(BUILD_DIR)/busybox
